24606215392c8d5f6bd41588ca9ec18c131c3809
Lucas Machado <machado.lucasalmeida@protonmail.com>
2026-03-14 21:57:17 -0300

    updating project week 01

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,9 @@
+const currentYearElement = document.querySelector('#currentyear');
+const today = new Date();
+currentYearElement.innerHTML = today.getFullYear();
 
 const lastMod = document.lastModified;
 const spanElement = document.getElementById("lastModified");
 if (spanElement) {
-    spanElement.textContent = `Last Modified: ${lastMod}`;
+    spanElement.textContent = `Last Modification: ${lastMod}`;
 }
